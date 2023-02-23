341d8614131d200319c3fcaa46d0624c50cd9e25
prarathod <itsmeprajwal2@gmail.com>
2023-02-23 23:54:04 +0530

    Test Commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,6 @@
 //@decs Gell all contacts
 //@route GET /api/contacts
 //@access public
-
 const getContact = (req, res) => {
     res.status(200).json({meddage:"Get all contact Prajwal"});
 }
@@ -9,17 +8,14 @@ const getContact = (req, res) => {
 //@decs Gell single contacts
 //@route GET /api/contacts/:id
 //@access public
-
 const getSingleContact = (req, res) => {
     res.status(200).json({meddage:`Get SIngle contact for ${req.params.id}`});
 }
 
 
-
 //@decs Creat new Contact
 //@route POST /api/contacts
 //@access public
-
 const createContact = (req, res) => {
     console.log(`user input value is :- ${req.body.name}`);
     const {name, age} = req.body;
@@ -32,7 +28,6 @@ const createContact = (req, res) => {
 //@decs Update Contact
 //@route PUT /api/contacts/:id
 //@access public
-
 const updateContact = (req, res) => {
     res.status(201).json({meddage:`Contact ${req.params.id} Updated sucssefully`});
 }
@@ -40,7 +35,6 @@ const updateContact = (req, res) => {
 //@decs Delete Contact
 //@route PUT /api/contacts/:id
 //@access public
-
 const deleteContact = (req, res) => {
     res.status(201).json({meddage:`Contact ${req.params.id} deleted sucssefully`});
 }
