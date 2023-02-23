ba43a0e39f19e106ea9ac5bfab40f85d70f71389
prarathod <itsmeprajwal2@gmail.com>
2023-02-23 23:30:43 +0530

    express router setup done

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('im in server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use('/api/contacts', require('./routes/contactRouts'));
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
